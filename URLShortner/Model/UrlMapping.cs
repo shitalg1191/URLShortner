ba3b34cf140e025c8849e3c56fa89d488e545959
Admin <Admin@DESKTOP-5SU1287>
2025-02-15 13:10:36 +0530

    done

---
+++ URLShortner/Model/UrlMapping.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortner.Model
+{
+    public class UrlMapping
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string shortUrl { get; set; } = string.Empty;
+        [Required]
+        public string longUrl { get; set; } = string.Empty;
+    }
+}
--- URLShortner/Service/UrlShortnerService.cs
+++ URLShortner/Service/UrlShortnerService.cs
@@ -1,16 +1,30 @@
 ﻿
+using URLShortner.Model;
+
 namespace URLShortner.Service
 {
     public class UrlShortnerService : IUrlShortnerService
     {
-        public Task<string> GetOrigionalUrlAsync(string shortUrl)
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private Random _random = new Random();
+        public Task<string> GetOrigionalUrlAsync(string shortcode)
         {
             throw new NotImplementedException();
         }
 
         public Task<string> ShortenUrlAsync(string origionalUrl)
         {
+            var shortcode = GenerateShortcode();
+            var shortUrl = "newgen.ly" + shortcode;
+            var mapping = new UrlMapping();
+            mapping.shortUrl = shortUrl;
+            mapping.longUrl = origionalUrl;
             throw new NotImplementedException();
+        }
+        private string GenerateShortcode(int length = 6)
+        {
+            return new string(Enumerable.Repeat(Alphabet, length).Select(s));
+
         }
     }
 }
