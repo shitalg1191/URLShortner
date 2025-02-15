92c26d3d2ffa5ab9a2415a49599f2b4c82a5e2e0
Admin <Admin@DESKTOP-5SU1287>
2025-02-15 12:24:03 +0530

    Commit Done

---
+++ URLShortner/Service/IUrlShortnerService.cs
@@ -0,0 +1,21 @@
+﻿namespace URLShortner.Service
+{
+    /// <summary>
+    /// Interface layer for URL service
+    /// </summary>
+    public interface IUrlShortnerService
+    {
+       /// <summary>
+       /// Method which will perform the shortning of the url provided
+       /// </summary>
+       /// <param name="OrigionalUrl">Instance of string</param>
+       /// <returns>Instance of string indicating shortned urlfor long url</returns>
+        Task<string>ShortenUrlAsync(string origionalUrl);
+        /// <summary>
+        /// Method will return the valid long url for given short url.
+        /// </summary>
+        /// <param name="shortUrl">Instance of string</param>
+        /// <returns></returns>
+        Task<string> GetOrigionalUrlAsync(string shortUrl);
+    }
+}
---
+++ URLShortner/Service/UrlShortnerService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace URLShortner.Service
+{
+    public class UrlShortnerService : IUrlShortnerService
+    {
+        public Task<string> GetOrigionalUrlAsync(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ShortenUrlAsync(string origionalUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
